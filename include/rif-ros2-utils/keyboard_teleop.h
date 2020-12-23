fd16b4fd248898e3e18d3a0db61b9a519e5a854b
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2020-12-23 13:14:31 -0400

    working node that publishes on cmd_vel

--- include/rif-ros2-utils/keyboard_teleop.h
+++ include/rif-ros2-utils/keyboard_teleop.h
@@ -10,30 +10,20 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp" // Allows you to use the most common pieces of the ROS2 system.
-#include "std_msgs/msg/string.hpp"
-
-using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+#include "geometry_msgs/msg/twist.hpp"
 
 class KeyboardTeleop : public rclcpp::Node
 {
 public:
-    KeyboardTeleop() : Node("keyboard_teleop"), count_(0) {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("sergiotest", 10);
-        timer_ = this->create_wall_timer(500ms, std::bind(&KeyboardTeleop::timer_callback, this));
+    KeyboardTeleop() : Node("keyboard_teleop") {
+        cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(topic_name, 1);
+        read_arrow_keys();
     }
 
 private:
-    void timer_callback();
-    /* void timer_callback() { */
-    /*     auto message = std_msgs::msg::String(); */
-    /*     message.data = "Hello, world! " + std::to_string(count_++); */
-    /*     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str()); */
-    /*     publisher_->publish(message); */
-    /* } */
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    std::string topic_name = "/cmd_vel";
+
+    void read_arrow_keys();
+
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
 };
--- src/keyboard_teleop.cpp
+++ src/keyboard_teleop.cpp
@@ -4,13 +4,56 @@
 // Node allows user to publish Twist commands to a topic (default: /cmd_vel).
 // #############################################################################
 
+#include <ncurses.h>
+
 #include "rif-ros2-utils/keyboard_teleop.h"
 
-void KeyboardTeleop::timer_callback() {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+void KeyboardTeleop::read_arrow_keys() {
+    int c;
+    auto message = geometry_msgs::msg::Twist();
+
+    // Initialize ncurses
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    addstr("Press arroy keys to move robot. Press 'q' to quit.\n");
+
+    do {
+        c = getch();
+        switch (c) {
+        case 259: // key up
+            message.linear.x = 0;
+            message.linear.y = 2;
+            message.linear.z = 0;
+            break;
+        case 260: // key left
+            message.linear.x = -2;
+            message.linear.y = 0;
+            message.linear.z = 0;
+            break;
+        case 261: // key right
+            message.linear.x = 2;
+            message.linear.y = 0;
+            message.linear.z = 0;
+            break;
+        case 258: // key down
+            message.linear.x = 0;
+            message.linear.y = -2;
+            message.linear.z = 0;
+            break;
+        default:
+            continue;
+            break;
+        }
+
+        // publish the message
+        cmd_vel_pub_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "Publishing: linear: x: '%.2f', y: '%.2f', z: '%.2f'",
+                    message.linear.x, message.linear.y, message.linear.z);
+    } while(c != 113); // lower-case q
+
+    endwin();
+    return;
 }
 
 // --------------------------
