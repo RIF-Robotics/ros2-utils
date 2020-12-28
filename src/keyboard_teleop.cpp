6da91e9d35c3d706e916c91fb4c9ab86e0a07df9
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2020-12-28 12:15:29 -0400

    publishing the correct commands

--- src/keyboard_teleop.cpp
+++ src/keyboard_teleop.cpp
@@ -14,6 +14,7 @@ KeyboardTeleop::KeyboardTeleop() : Node("keyboard_teleop") {
 }
 
 void KeyboardTeleop::read_arrow_keys() {
+    char buff [50];
     int c;
     auto message = geometry_msgs::msg::Twist();
 
@@ -21,30 +22,52 @@ void KeyboardTeleop::read_arrow_keys() {
     initscr();
     keypad(stdscr, TRUE);
     noecho();
-    addstr("Press arroy keys to move robot. Press 'q' to quit.\n");
+    sprintf(buff, "Publishing to topic: %s.\n", topic_name.c_str());
+    addstr(buff);
+    addstr("Press arroy keys to move robot. Press 's' to STOP. Press 'q' to QUIT.\n");
 
     do {
         c = getch();
         switch (c) {
-        case 259: // key up
-            message.linear.x = 0;
-            message.linear.y = 2;
+        case 259: // key up | forward
+            message.linear.x = 1;
+            message.linear.y = 0;
             message.linear.z = 0;
+            message.angular.x = 0;
+            message.angular.y = 0;
+            message.angular.z = 0;
             break;
         case 260: // key left
-            message.linear.x = -2;
+            message.linear.x = 0;
             message.linear.y = 0;
             message.linear.z = 0;
+            message.angular.x = 0;
+            message.angular.y = 0;
+            message.angular.z = 1;
             break;
         case 261: // key right
-            message.linear.x = 2;
+            message.linear.x = 0;
             message.linear.y = 0;
             message.linear.z = 0;
+            message.angular.x = 0;
+            message.angular.y = 0;
+            message.angular.z = -1;
             break;
-        case 258: // key down
-            message.linear.x = 0;
-            message.linear.y = -2;
+        case 258: // key down | backward
+            message.linear.x = -1;
+            message.linear.y = 0;
             message.linear.z = 0;
+            message.angular.x = 0;
+            message.angular.y = 0;
+            message.angular.z = 0;
+            break;
+        case 115: // lower-case s | STOP
+            message.linear.x = 0;
+            message.linear.y = 0;
+            message.linear.z = 0;
+            message.angular.x = 0;
+            message.angular.y = 0;
+            message.angular.z = 0;
             break;
         default:
             continue;
@@ -53,10 +76,12 @@ void KeyboardTeleop::read_arrow_keys() {
 
         // publish the message
         cmd_vel_pub_->publish(message);
-        RCLCPP_INFO(this->get_logger(), "Publishing: linear: x: '%.2f', y: '%.2f', z: '%.2f' \r",
+        RCLCPP_INFO(this->get_logger(), "Publishing: linear: x: '%.2f', y: '%.2f', z: '%.2f'\r",
                     message.linear.x, message.linear.y, message.linear.z);
+        RCLCPP_INFO(this->get_logger(), "Publishing: angular: x: '%.2f', y: '%.2f', z: '%.2f'\r\n",
+                    message.angular.x, message.angular.y, message.angular.z);
 
-    } while(c != 113); // lower-case q
+    } while(c != 113); // lower-case q | QUIT
 
     endwin();
     return;
