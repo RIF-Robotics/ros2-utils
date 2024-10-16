1cf553cca5a6df813e5b06585ce68279348d6133
Kevin DeMarco <kevin@kevindemarco.com>
2024-10-16 13:29:59 -0400

    use cv_bridge.hpp

--- src/compress_image.cpp
+++ src/compress_image.cpp
@@ -4,7 +4,7 @@
 #include <sensor_msgs/msg/compressed_image.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 
 class CompressImage : public rclcpp::Node
 {
